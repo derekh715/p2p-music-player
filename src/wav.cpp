923b10fdbd42a39837414cb9f8b5be8f06c1e003
antony6212 <106349561+antony6212@users.noreply.github.com>
2023-04-25 20:08:16 +0800

    Update wav.cpp

--- src/wav.cpp
+++ src/wav.cpp
@@ -1,96 +1,183 @@
-// CSCI3280 Phase 1
-// Thomas + Anthony
-
 #include "wav.h"
 
 struct WavHeaderCDT {
-    uint32_t RIFF_ChunkID;
-    uint32_t RIFF_ChunkSize;
-    uint32_t RIFF_Format;
+    uint16_t AudioFormat;
+    uint16_t NumChannels;
+    uint32_t SampleRate;
+    uint32_t ByteRate;
+    uint16_t BlockAlign;
+    uint16_t BitsPerSample;
 
-    uint32_t fmt_Subchunk1ID;
-    uint32_t fmt_Subchunk1Size;
-    uint16_t fmt_AudioFormat;
-    uint16_t fmt_NumChannels;
-    uint32_t fmt_SampleRate;
-    uint32_t fmt_ByteRate;
-    uint16_t fmt_BlockAlign;
-    uint16_t fmt_BitsPerSample;
-
-    uint32_t data_Subchunk2ID;
-    uint32_t data_Subchink2Size;
+    uint32_t dataSize;
 };
 
 Wav::Wav() {
     WavHeader = new WavHeaderCDT;
 }
 
-Wav::Wav(const char* FilePath) {
+Wav::Wav(const char *path) {
     WavHeader = new WavHeaderCDT;
-    openWavFile(FilePath);
+    openWavFile(path);
 }
 
-bool Wav::openWavFile(const char* FilePath) {
-    std::ifstream fin;
-    fin.open(FilePath, std::ios::binary);
-    if (!fin.is_open()) {
-        std::cerr << "Cannot Open File: " << FilePath << std::endl;
-        return false;
+bool Wav::openWavFile(const char *path){
+    // create an empty one to init.
+    WavHeaderCDT init = WavHeaderCDT();
+    *WavHeader = init;
+    // delete if the Wav is reused (size is different)
+    if(data != nullptr)
+        delete data;
+
+    std::ifstream fin(path, std::ios::binary);
+
+    if(!fin.is_open()){
+        std::cerr << "Cannot Open File: " << path << std::endl;
+        goto fail;  // file not exist
     }
+
     fin.seekg(0);
-    if (!fin.read((char*)WavHeader, sizeof(WavHeaderCDT))) {
-        std::cerr << "Cannot Read File: " << FilePath << std::endl;
-        return false;
+    {// read RIFF chunk descriptor, Subchunk1ID
+        const char descriptor[] = "RIFF....WAVEfmt ";   // .... is chunkSize
+        for(int field=0; field<4; field++){
+            for(int i=0; i<4; i++){
+                unsigned char c;
+                fin.read(reinterpret_cast<char*>(&c),1);
+                if(field == 1){
+                    // chunkSize += (uint32_t)c << (i*8);  // not reading as it's not related to the audio
+                }else{
+                    if(c != descriptor[field*4+i]){ // not match with "RIFF" / "WAVE" / "fmt "
+                        std::cerr << "Not Canonical WAV File: " << path << std::endl;
+                        goto fail;
+                    }
+                }
+            }
+        }
     }
-    if (
-        WavHeader->RIFF_ChunkID != (uint32_t)0x46464952
-        || WavHeader->RIFF_Format != (uint32_t)0x45564157
-        || WavHeader->fmt_Subchunk1ID != (uint32_t)0x20746d66
-        || WavHeader->fmt_AudioFormat != 1
-        // || WavHeader->data_Subchunk2ID != (uint32_t)0x61746164
-        ) {
-        std::cerr << "Not Canonical WAV File: " << FilePath << std::endl;
-        return false;
-    }
+    {// read fmt sub-chunk (subchunk1)
+        // read Subchunk1Size
+        uint32_t Subchunk1Size = 0;
+        for(int i = 0; i<4; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            Subchunk1Size += (uint32_t)c << (i*8);
+        }
+        uint32_t readCount = 0;
 
-    if (data == nullptr)
-        data = (char*)malloc(WavHeader->data_Subchink2Size);
-    else
-        data = (char*)realloc(data, WavHeader->data_Subchink2Size);
-    if (!fin.read((char*)data, WavHeader->data_Subchink2Size)) {
-        std::cerr << "Cannot Read File Data: " << FilePath << std::endl;
-        return false;
-    }
+        // read AudioFormat (2 byte)
+        for(int i = 0; i<2; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->AudioFormat += (uint16_t)c << (i*8);
+            readCount++;
+        }
+        if(WavHeader->AudioFormat != 1){    // not integer PCM
+            std::cerr << "Not Canonical WAV File: " << path << std::endl;
+            goto fail;
+        }
+        
+        // read NumChannels (2 byte)
+        for(int i = 0; i<2; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->NumChannels += (uint16_t)c << (i*8);
+            readCount++;
+        }
 
-    fin.close();
+        // read SampleRate (4 byte)
+        for(int i = 0; i<4; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->SampleRate += (uint32_t)c << (i*8);
+            readCount++;
+        }
+
+        // read ByteRate (4 byte)
+        for(int i = 0; i<4; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->ByteRate += (uint32_t)c << (i*8);
+            readCount++;
+        }
+
+        // read BlockAlign (2 byte)
+        for(int i = 0; i<2; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->BlockAlign += (uint16_t)c << (i*8);
+            readCount++;
+        }
+
+        // read BitsPerSample (2 byte)
+        for(int i = 0; i<2; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->BitsPerSample += (uint16_t)c << (i*8);
+            readCount++;
+        }
+        
+        // jump to subchunk2 in case (Subchunk1Size != 16)
+        fin.seekg(Subchunk1Size - readCount,std::ios::cur);
+    }// end of fmt sub-chunk (subchunk1)
+    
+    {// read Subchunk2ID, Size
+        // read ID
+        const char id[] = "data";
+        for(int i=0; i<4; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            if(c != id[i]){ // cannot find "data"
+                std::cerr << "Not Canonical WAV File: " << path << std::endl;
+                goto fail;
+            }
+        }
+
+        // read Subchunk2Size
+        for(int i = 0; i<4; i++){
+            unsigned char c;
+            fin.read(reinterpret_cast<char*>(&c),1);
+            WavHeader->dataSize += (uint32_t)c << (i*8);
+        }
+    }
+    
+    // read data
+    data = new char[WavHeader->dataSize];
+    if (!fin.read((char*)data, WavHeader->dataSize)) {
+        std::cerr << "Cannot Read File Data: " << path << std::endl;
+        goto fail;
+    }
+    
+    // fin.close();    // fin will auto destruct after return (should be)
     return true;
+
+    fail:   // fail to open wav file
+        // fin.close();    // fin will auto destruct after return (should be) :)
+        return false;
 }
 
 unsigned int Wav::getNumChannels() {
-    return WavHeader->fmt_NumChannels;
+    return WavHeader->NumChannels;
 }
 
 unsigned int Wav::getSampleRate() {
-    return WavHeader->fmt_SampleRate;
+    return WavHeader->SampleRate;
 }
 
 unsigned int Wav::getByteRate() {
-    return WavHeader->fmt_ByteRate;
+    return WavHeader->ByteRate;
 }
 unsigned int Wav::getBlockAlign() {
-    return WavHeader->fmt_BlockAlign;
+    return WavHeader->BlockAlign;
 }
 unsigned int Wav::getBitsPerSample() {
-    return WavHeader->fmt_BitsPerSample;
+    return WavHeader->BitsPerSample;
 }
 unsigned int Wav::getSubchunk2Size() {
-    return WavHeader->data_Subchink2Size;
+    return WavHeader->dataSize;
 }
 const char* Wav::getData() {
     return data;
 }
 
 unsigned int Wav::getDataPos(unsigned int TimeInMilliseconds) {
-    return TimeInMilliseconds / 1000 * WavHeader->fmt_ByteRate;
+    return TimeInMilliseconds / 1000 * WavHeader->ByteRate;
 }
-
